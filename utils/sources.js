ed336ba2c5250a5b806aeed852ecef80d2137d8b
snee-a <pandeysneha953@gmail.com>
2025-07-11 11:40:07 +0530

    added all the apis

---
+++ utils/sources.js
@@ -0,0 +1,17 @@
+module.exports=[
+    {
+        name:"flipkart",url:"https://fakestoreapi.com/products"
+    },
+    {
+      name:"Amazon",url:"https://dummyjson.com/products"  
+    },
+    {
+        name:"meesho",url:"https://api.escuelajs.co/api/v1/products"
+    },
+    {
+        name:"savana", url:"https://fake-store-api.mock.beeceptor.com/api/products"
+    },
+    {
+        name:"urbanic",url:"https://dummyjson.com/products?limit=100"
+    }
+]
