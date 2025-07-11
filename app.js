4da363b1e96a2c1a87e2d0ef4b0aa59a79d0b2ce
snee-a <pandeysneha953@gmail.com>
2025-07-11 11:34:25 +0530

    INitail setup forming

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const cors=require("cors");
+
+const productRoutes=require("./routes/productRoutes");
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/",productRoutes);
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`Listening at the port ${PORT}`);
+})
